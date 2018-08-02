f64a20efa4eded45fbb1efd1f45f8e0256cd368c
Ivan <ivanvlora@gmail.com>
2018-08-02 13:25:50 +0900

    updated db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-let db = {
-    localhost: 'mongodb://ivan:redsox24@ds163781.mlab.com:63781/node-todo-api',
-    mlab: 'mongodb://localhost:27017/TodoApp'
-};
-
-mongoose.connect(process.env.PORT ? db.localhost || db.mlab, {
+mongoose.connect(process.env.MONGODB_JADE_URI ||'mongodb://localhost:27017/TodoApp', {
     useNewUrlParser: true
 });
 
