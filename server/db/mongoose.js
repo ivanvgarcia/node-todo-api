5de35958bd41320692abcf13904e82160c82b366
Ivan <ivanvlora@gmail.com>
2018-08-02 13:06:59 +0900

    Updated db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://ivan:redsox24@ds163781.mlab.com:63781/node-todo-api' || 'mongodb://localhost:27017/TodoApp', {
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
     useNewUrlParser: true
 });
 
